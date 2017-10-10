5d22651b8138f5996571201b64e1c89be271f872
Exar-Kun <matthew.harvey@grad.moore.sc.edu>
2017-10-10 13:35:39 -0400

    updates

--- PSets/Econ815_PS4.py
+++ PSets/Econ815_PS4.py
@@ -12,7 +12,6 @@ import math
 from statsmodels.iolib.summary2 import summary_col
 from geopy.distance import vincenty
 
-
 ##############################################################################
 #########################Reading In The Data and Preliminaries################
 ##############################################################################
@@ -36,6 +35,12 @@ data['target_coordinates'] = list(zip(data.target_lat, data.target_long))
 
 data.head(n=3)
 
+#We now drop the longitude and latitude columns as we now have full cordinate
+#columns.
+data = data[['buyer_id','num_stations_buyer', 'corp_owner_buyer', 
+             'buyer_coordinates', 'target_id','hhi_target', 'price', 
+             'population_target', 'target_coordinates', 'year']]
+
 #We now divide this data by year lest we have horrible trouble with the
 #creation of counterfactuals
 data2007 = data[(data['year']==2007)] 
@@ -43,11 +48,16 @@ data2007 = data[(data['year']==2007)]
 data2008 = data[(data['year']==2008)]
 
 
-data2007b = data2007[['buyer_id','num_stations_buyer', 'corp_owner_buyer', 'buyer_coordinates']]
-data2007t = data2007[['target_id','hhi_target', 'price', 'population_target', 'target_coordinates']]
+data2007b = data2007[['buyer_id','num_stations_buyer', 'corp_owner_buyer', 
+                      'buyer_coordinates', 'price']]
+data2007t = data2007[['target_id','hhi_target', 'population_target', 
+                      'target_coordinates', 'year']]
 
-data2008b = data2008[['buyer_id','num_stations_buyer', 'corp_owner_buyer', 'buyer_coordinates']]
-data2008t = data2008[['target_id','hhi_target', 'price', 'population_target', 'target_coordinates']]
+data2008b = data2008[['buyer_id','num_stations_buyer', 'corp_owner_buyer', 
+                      'buyer_coordinates', 'price']]
+data2008t = data2008[['target_id','hhi_target', 'population_target', 
+                      'target_coordinates', 'year']]
+
 #Upon the generation of the counterfactuals we will use this to create
 #distance data
 
@@ -66,12 +76,33 @@ for i in range(len(data2007b)):
                     [data2007b.iloc[[i]].reset_index(drop=True), 
                     data2007t.iloc[[j]].reset_index(drop=True)], axis=1))
 
-#Having now created the data we seek, we combine it into 
+#Having now created the data we seek, we combine it with the initial dataframe
+full_data = pd.concat([data, counter])
 
+#This being completed we reindex the dataframe 
+full_data = full_data.reset_index(drop=True)
 
-    
-
+#A trial of the index
+full_data.buyer_coordinates[0]
 
 ##############################################################################
 ####################################Distance Data#############################
 ##############################################################################
+
+
+#The data now being generated we move on to the creation of distance data
+full_data['distance'] = [vincenty(full_data.buyer_coordinates.iloc[i], 
+                                  full_data.target_coordinates.iloc[i]).miles 
+                                    for i in range(len(full_data))]
+
+#We now print the data to ensure that we are doing things rightly
+full_data.head(n=3)
+
+##############################################################################
+##########################The Max Score Estimator#############################
+##############################################################################
+
+#Finally, having produced the relevant data, we move on to the creation of 
+#the max score estimator
+
+
